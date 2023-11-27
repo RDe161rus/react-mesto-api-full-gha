0a9ca3118a4c4bd9778d6cefe434cf018ea13e28
Denis Rastrepin <rusBeliy161@yandex.ru>
2023-11-27 12:19:14 +0300

    feat: logger

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,17 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const appRouter = require('./routes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1/mestodb' } = process.env;
-
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
-mongoose.connect(DB_URL);
-
+mongoose.connect(MONGO_URL);
+app.use(requestLogger);
 app.use(appRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
